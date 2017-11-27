3bb78c6bf29002d24ca366596b649a399335cdce
birodb <botond23@gmail.com>
2017-11-27 10:23:11 +0100

    update proxy.py
    use a dictionary to store the uri->local_file_name mappings
    use numbers for local file names

--- proxy.py
+++ proxy.py
@@ -8,35 +8,50 @@ import urllib.error
 import os
 import os.path
 import shutil
-import base64
-#import zlib
-import hashlib
+import json
 
 PORT = 8080
 BASE_PATH = os.path.abspath('proxy_cache')
-
 def ensure_dir(dir_name):
     """create dir if doesn't exists"""
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
+def abs_path(fname):
+    """convert to abs path relative to base path"""
+    return os.path.join(BASE_PATH, fname)
+
+class NameMap:
+    def __init__(self, fname):
+        self.data = {}
+        self.fname = fname
+
+    def load(self):
+        try:
+            with open(self.fname, "rt") as f:
+                self.data = json.load(f)
+        except (IOError) as e:
+            print("proxy cache file not exists yet")
+
+    def save(self):
+        with open(self.fname, "w+t") as f:
+            json.dump(self.data, f, indent=2, sort_keys=True)
+    
+    def as_local(self, path):
+        """remap the http uri to a local file(number)"""
+        return abs_path(self.data.setdefault(path, str(len(self.data))))
+
+NAME_MAP = NameMap(abs_path('proxy.json'))
+        
 class Proxy(http.server.SimpleHTTPRequestHandler):
     """the caching proxy class"""
-    def as_local_path(self):
-        """convert the url to a local file path"""
-        #return os.path.join(BASE_PATH, urllib.parse.quote_plus(self.path))
-        bin_data = self.path.encode('UTF-8')
-        #compr_data = zlib.compress(bin_data, 9)
-        compr_data = hashlib.sha256(bin_data).digest()
-        local_name = base64.urlsafe_b64encode(compr_data).decode('ASCII')
-        local_name = local_name.split("=")[0]
-        return os.path.join(BASE_PATH, local_name)
 
     def do_GET(self):
         """handle the GET request"""
-        local_fname = self.as_local_path()
+        local_fname = NAME_MAP.as_local(self.path)
         if not os.path.isfile(local_fname):
             #file doesn't exists, create it
+            NAME_MAP.save()
             print("caching: ", self.path, local_fname)
             try:
                 with open(local_fname, "wb") as f_cache:
@@ -63,6 +78,8 @@ class Proxy(http.server.SimpleHTTPRequestHandler):
 
 if __name__ == "__main__":
     ensure_dir(BASE_PATH)
+    NAME_MAP.load()
+
     print("serving at port: ", PORT, " base path ", BASE_PATH)
-    httpd = socketserver.ForkingTCPServer(('', PORT), Proxy)
+    httpd = socketserver.TCPServer(('', PORT), Proxy)
     httpd.serve_forever()
